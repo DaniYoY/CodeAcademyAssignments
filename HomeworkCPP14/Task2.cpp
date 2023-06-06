d2d6e905ca4a23b74b9bfc5567db58ca291b1c27
DaniYoY <130496759+DaniYoY@users.noreply.github.com>
2023-06-06 21:40:05 +0300

    HW 14 task2 finalized

--- HomeworkCPP14/Task2.cpp
+++ HomeworkCPP14/Task2.cpp
@@ -1,3 +1,41 @@
 // Задача 02
 // Дефинирайте вектор и напишете цикъл, в който добавяте елементи и принтирате размера и капацитета на всяка итерация. Направете
 // същото, но преди цикъла използвайте reserve.
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+void observeVectorGrowth()
+{
+    std::vector<std::string> vec;
+    std::string line;
+    while(!std::getline(std::cin, line).eof()){
+        vec.push_back(line);
+        std::cout << "Size is " << vec.size()
+                << " and capacity is " << vec.capacity() 
+                << std:: endl;
+    }
+    return;
+}
+
+void observeVectorGrowthWithReserve(size_t n)
+{
+    std::vector<std::string> vec;
+    std::string line;
+    vec.reserve(n);
+    while(!std::getline(std::cin, line).eof()){
+        vec.push_back(line);
+        std::cout << "Size is " << vec.size()
+                << " and capacity is " << vec.capacity() 
+                << std:: endl;
+    }
+    return;
+}
+
+int main(int argc, char const *argv[])
+{
+    observeVectorGrowth();
+    observeVectorGrowthWithReserve(100);
+    return 0;
+}
