060195e59fb45aaa4efc85daeabff2750b2eb06a
DaniYoY <130496759+DaniYoY@users.noreply.github.com>
2023-09-10 16:27:55 +0300

    HW 5 task 6

--- PythonLectures/HomeworkPy05/Task6.py
+++ PythonLectures/HomeworkPy05/Task6.py
@@ -3,6 +3,8 @@
 # '{', '}', '[', and ']' is valid. The brackets must close in the correct order.
 # Example Input: "{[]}"
 # Example Output: True
+import unittest
+
 
 def validate_parentheses(string: str) -> bool:
     if string is None or string == '':
@@ -19,3 +21,46 @@ assert validate_parentheses('{[]}') is True
 assert validate_parentheses('') is False
 # if string is invalid
 assert validate_parentheses('{[a]}') is False
+
+
+# version 2
+def validate_parentheses_v2(string: str) -> bool:
+    if not isinstance(string, str):
+        raise ValueError('this is not a str')
+    stack = list()
+    parentheses_pair = {']': '[', '}': '{', ')': '('}
+    for ch in string:
+        if ch in '{[(':
+            stack.append(ch)
+        elif ch in '}])':
+            if not stack:
+                return False
+            if parentheses_pair[ch] == stack[-1]:
+                stack.pop()
+        else:
+            return False
+
+        if not stack and string:
+            return True
+        else:
+            return False
+
+
+class TestParenthesesValidator(unittest.TestCase):
+    def test_if_isinstance_raises_error(self):
+        self.assertRaises(ValueError, validate_parentheses_v2, 2)
+
+    def test_if_False_when_string_empty(self):
+        self.assertFalse(validate_parentheses_v2(''))
+
+    def test_if_stack_not_emptied(self):
+        self.assertFalse(validate_parentheses_v2('{{}'))
+
+    def test_if_string_contains_other_chars(self):
+        self.assertFalse(validate_parentheses_v2('{{d}}'))
+
+    def test_if_string_start_with_back_parenthesis(self):
+        self.assertFalse(validate_parentheses_v2(']'))
+
+    def test_if_parentheses_match(self):
+        self.assertFalse(validate_parentheses_v2('{{[()]}}'))
